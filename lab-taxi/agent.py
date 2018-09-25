757ee8f0ee80199405bbe6979a9dfed600cf3c0f
Federico Salvioli <salvioli.federico@gmail.com>
2018-09-25 21:34:13 +0200

    [up] implemented qlearning and expected sarsa

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,9 +1,16 @@
 import numpy as np
 from collections import defaultdict
+from enum import Enum, auto
+
+class AlgorithmType(Enum):
+    MonteCarlo = auto()
+    Sarsa = auto()
+    QLearning = auto()
+    ExpectedSarsa = auto()
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, type=AlgorithmType.ExpectedSarsa):
         """ Initialize agent.
 
         Params
@@ -12,6 +19,13 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.start_epsilon = 0.8
+        self.epsilon = self.start_epsilon
+        self.epsilon_decay = lambda e: e*self.start_epsilon
+        self.alpha = 0.1
+        self.gamma = 0.8
+        self.episode_counter = 0
+        self.algorithm_type = type
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +38,8 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        greedy_action = np.argmax(self.Q[state])
+        return greedy_action if np.random.uniform() > self.epsilon else np.random.randint(0, self.nA)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +52,20 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+
+        if self.algorithm_type == AlgorithmType.QLearning:
+            # Q-learning
+            self.Q[state][action] += self.alpha * (reward + self.gamma*np.max(self.Q[next_state]) - self.Q[state][action])
+
+        elif self.algorithm_type == AlgorithmType.ExpectedSarsa:
+            ## Expected Sarsa
+            pi = np.ones(self.nA) * self.epsilon / self.nA
+            pi[np.argmax(self.Q[state])] += 1 - self.epsilon
+            self.Q[state][action] += self.alpha * (reward + self.gamma * np.sum(pi * self.Q[next_state]) - self.Q[state][action])
+        else:
+            raise ValueError("Algorithm type %s is not supported" % (self.algorithm_type))
+
+
+        if done:
+            self.episode_counter += 1
+            self.epsilon = self.epsilon_decay(self.epsilon)
